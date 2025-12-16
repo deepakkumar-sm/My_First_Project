6be2579844c0af28a5b982541011af5a732ee6da
deepakkumar-sm <smridee@outlook.com>
2025-12-16 20:04:22 +0530

    new learnings

--- learning.py
+++ learning.py
@@ -1,34 +1,22 @@
-# # import math
-
-# # def life_in_weeks(age):
-# #     weeks_left = math.floor(90-age)*52
-# #     print(f"You have {weeks_left} weeks left.")
-
-# # life_in_weeks(38)
-
-# def greet_with_name(name, location):
-#     print(f"Hello {name}")
-#     print(f"what is it like in {location}")
-
-# greet_with_name("Deepak Kumar", "New Delhi")
-
 def calculate_love_score(name1, name2):
     combined = (name1 + name2).lower()
-    
+
     true_count = 0
     for letter in "true":
         true_count += combined.count(letter)
-    print(true_count)
 
     true_love = 0
-    for letter in "love":
-        true_love += combined.count(letter)
-    print(true_love)
+    for word in "love":
+        true_love += combined.count(word)
 
     score = str(true_count) + str(true_love)
-    print(score)
+    print(f"Your Love Score is: {score}") 
 
-calculate_love_score("smriti","Deepak")
+
+
+
+calculate_love_score("vijai", "ratna")
+
 
 
 
