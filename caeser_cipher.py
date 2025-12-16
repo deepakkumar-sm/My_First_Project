ae128c93341e9a367c62ed13e24d2d53c4d4be67
deepakkumar-sm <smridee@outlook.com>
2025-12-17 01:05:10 +0530

    some changes

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -27,15 +27,10 @@ def caeser(original_text, shift_amount, encode_or_decode):
 should_continue = True
 
 while should_continue:
-    
-
 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-
-    
-
 
     caeser(text, shift, direction)
 
