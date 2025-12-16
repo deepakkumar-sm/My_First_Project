272de3866ca5ec5e72ffecb9d2194b71bbf016c1
deepakkumar-sm <smridee@outlook.com>
2025-12-17 00:25:06 +0530

    new learnings

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,26 +1,47 @@
+from art import logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+
+# Type 'yes' if you want to go again. Otherwise, type 'no'.
+
+def caeser(original_text, shift_amount, encode_or_decode):
+    cipher_text = ''
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
 
-    print(f"Here is the encoded result: {cipher_text}")
+        if letter not in alphabet:
+            cipher_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
 
-encrypt(text, shift)
+    print(f"Here is your {encode_or_decode}d result: {cipher_text}")
+
+should_continue = True
+
+while should_continue:
+    
 
 
-# TODO-1: Create a function called 'encrypt()' that takes
-#  'original_text' and 'shift_amount' as 2 inputs.
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    
 
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter
-#  of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+    caeser(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye, thanks for playing")
+
 
